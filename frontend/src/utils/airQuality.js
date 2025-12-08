e73c801e21745131514a2c31a4a12e75a3fbbc53
m1l3s3n <141837382+M1l3s3n@users.noreply.github.com>
2025-12-08 19:58:50 +0200

    fix colors

--- frontend/src/utils/airQuality.js
+++ frontend/src/utils/airQuality.js
@@ -1,8 +1,8 @@
 export const getAirQuality = (co2) => {
   if (!co2 || co2 < 400)
     return { color: "#78FF8A", label: "Відмінна", level: "good" };
-  if (co2 < 600) return { color: "#D6F01F", label: "Добра", level: "moderate" };
-  if (co2 < 1000)
+  if (co2 < 800) return { color: "#D6F01F", label: "Добра", level: "moderate" };
+  if (co2 < 1200)
     return { color: "#FFB84D", label: "Помірна", level: "unhealthy" };
-  if (co2 < 2000) return { color: "#FF5C5C", label: "Погана", level: "bad" };
+  if (co2 < 6999) return { color: "#FF5C5C", label: "Погана", level: "bad" };
 };
